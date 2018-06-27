c4c47ff182a56a32b1d537d3379d9b208a3686fe
JoeW3947 <fakewildfong@gmail.com>
2018-06-27 01:36:16 -0400

    For some reason, xampp won't update this file on saves in eclipse. Just checking if this works.

---
+++ w/js/appointments.js
@@ -0,0 +1,30 @@
+window.addEventListener('domReady', function() {
+	debugger;
+	var x = document.createElement('img');
+	x.src = 'https://cdn1.iconfinder.com/data/icons/pixel-perfect-at-16px-volume-2/16/5001-128.png';
+	x.className = 'drop-arrow';
+	var z = document.getElementsByClassName('day');
+	for(y = 0; y < z.length; y++) {
+		var w = x.cloneNode();
+		var e = z[y].firstChild;
+		z[y].insertBefore(w, e);
+		w.onclick = rotateMe;
+		e.onclick = rotateMe;
+	} 
+});
+function rotateMe(e) {
+	e.preventDefault();
+	this.parentElement.classList.toggle('collapsed');
+}
+function expand() {
+	var days = document.getElementsByClassName('day');
+	for (var loop = 0; loop < days.length; loop++) {
+		days[loop].classList.remove('collapsed');
+	}
+}
+function collapse() {
+	var days = document.getElementsByClassName('day');
+	for (var loop = 0; loop < days.length; loop++) {
+		days[loop].classList.add('collapsed');
+	}
+}
