941d8d8baa9b629110bd4c83371be69fa018e044
JoeW3947 <fakewildfong@gmail.com>
2018-07-17 20:40:22 -0400

    Added an easy tooltip library.

---
+++ w/js/tooltip.js
@@ -0,0 +1,24 @@
+var elements = document.querySelectorAll("[data-tooltip]");
+var template = document.createElement("span");
+template.classList.add("tooltiptext");
+for (var x of elements.values()) {
+	x.classList.add("tooltip");
+	template.innerHTML = x.dataset.tooltip;
+	x.insertBefore(template.cloneNode(true), x.firstElementChild);
+	x.tooltip = x.firstElementChild;
+	x.tooltip.style.visibility = 'hidden';
+	x.onmouseenter = function() {
+		var store = this;
+		this.fade = setTimeout(function() {store.tooltip.style.opacity = 1;}, 600);
+		this.tooltip.style.visibility = 'visible';
+	};
+	x.onmouseleave = function() {
+		clearTimeout(this.fade);
+		this.tooltip.style.opacity = 0;
+		this.tooltip.addEventListener("transitionend", unShow);
+	};
+}
+function unShow() {
+	this.style.visibility = 'hidden';
+	this.removeEventListener("transitionend", unShow);
+}
