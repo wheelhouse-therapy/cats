231520c7f7e9ed9d7a987cb6f36a2585cd6542a4
JoeW3947 <fakewildfong@gmail.com>
2018-08-06 20:45:32 -0400

    GAAAAAAAAAAAAAH!

--- w/js/tooltip.js
+++ w/js/tooltip.js
@@ -1,21 +1,21 @@
 var elements = document.querySelectorAll("[data-tooltip]");
 var template = document.createElement("span");
-template.classList.add("tooltiptext");
+template.classList.add("catstooltiptext");
 for (var x of elements.values()) {
-	x.classList.add("tooltip");
+	x.classList.add("catstooltip");
 	template.innerHTML = x.dataset.tooltip;
 	x.insertBefore(template.cloneNode(true), x.firstElementChild);
-	x.tooltip = x.firstElementChild;
-	x.tooltip.style.visibility = 'hidden';
+	x.catstooltip = x.firstElementChild;
+	x.catstooltip.style.visibility = 'hidden';
 	x.onmouseenter = function() {
 		var store = this;
-		this.fade = setTimeout(function() {store.tooltip.style.opacity = 1;}, 600);
-		this.tooltip.style.visibility = 'visible';
+		this.fade = setTimeout(function() {store.catstooltip.style.opacity = 1;}, 600);
+		this.catstooltip.style.visibility = 'visible';
 	};
 	x.onmouseleave = function() {
 		clearTimeout(this.fade);
-		this.tooltip.style.opacity = 0;
-		this.tooltip.addEventListener("transitionend", unShow);
+		this.catstooltip.style.opacity = 0;
+		this.catstooltip.addEventListener("transitionend", unShow);
 	};
 }
 function unShow() {
