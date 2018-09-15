58ccbed03e4cc7e0f19c4366af2820c067f5b968
JoeW3947 <fakewildfong@gmail.com>
2018-09-15 18:56:59 -0400

    Made the auto-advance work for the datalist.

--- w/js/assessments.js
+++ w/js/assessments.js
@@ -1,15 +1,19 @@
-var chars = ["n", "s", "o", "f"]
+var chars = ["a", "s", "o", "n"]
 addEventListener("DOMContentLoaded", function() {
 	var inputs = document.body.querySelectorAll("input.score-item");
 	
 	inputs.forEach(function(a, b){
-		a.addEventListener("keypress", function() {
+		a.addEventListener("keydown", function() {
 			if (checkInput(event, this)) {
 				try {
 					inputs[b + 1].focus();
 				} catch(e) {}
 			}
 		});
+		a.addEventListener("input", function() {
+			inputs[b + 1].focus();
+		})
+		a.addEventListener("paste", function(e) {e.preventDefault();});
 	});
 });
 	
