b34b103096ce542ffdcfa6a4f4f83063f88996be
elw-arduino <minecraftisawsome007+github@gmail.com>
2019-06-06 12:42:20 -0400

    Fixed Support system

--- jx.php
+++ jx.php
@@ -63,7 +63,7 @@ switch( $cmd ) {
     case 'support':
         $people = (new PeopleDB($oApp))->getKFRCond("P","uid='{$oApp->sess->GetUID()}'");
         if($people && $email = $people->Value("email")){
-            $rJX['bOk'] = mail("developer@catherapyservices.ca",SEEDInput_Str('support')." from ".$people->Expand("[[first_name]] [[last_name]]"),SEEDInput_Str('supportDesc'),"From: ".$email);
+            $rJX['bOk'] = mail("developer@catherapyservices.ca",SEEDInput_Str('supportType')." from ".$people->Expand("[[first_name]] [[last_name]]"),SEEDInput_Str('supportDesc'),"From: ".$email);
         }
         else {
             $rJX['sErr'] = "You must have an associated person record with an email address set in order to use the support request feature";
