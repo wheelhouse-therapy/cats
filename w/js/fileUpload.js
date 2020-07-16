c59810b3ea2a1ae6bbd38aee2c7f4bbbb7dcef9d
JoeWildfong <wildfong.joe@gmail.com>
2020-07-16 16:26:01 -0400

    Improve file upload with progress bar and cancel button.

--- w/js/fileUpload.js
+++ w/js/fileUpload.js
@@ -1,8 +1,38 @@
+var currentUpload;
 function submitForm(e){
 	var formData = new FormData(document.getElementById('upload-file-form'));
 	formData.append("cmd","therapist-resource-upload");
-	e.currentTarget.parentElement.innerHTML = "<button><i class='fa fa-sync-alt fa-spin'></i> Uploading</button>";
-	$.ajax({
+	e.currentTarget.parentElement.innerHTML = "<button id='uploading-button'><i class='fa fa-sync-alt fa-spin'></i> <span id='uploading-text'>Uploading</span></button>";
+	currentUpload = $.ajax({
+		xhr: function() {
+			var xhr = new window.XMLHttpRequest();
+			
+			document.getElementById("upload-bar").classList.add("uploading");
+			document.getElementById("filled-bar").style.width = "0";
+			var uploadingButton = document.getElementById("uploading-button");
+			uploadingButton.addEventListener("mouseover", function() {
+				this.lastElementChild.innerHTML = "Cancel";
+			});
+			uploadingButton.addEventListener("mouseout", function() {
+				this.lastElementChild.innerHTML = "Uploading";
+			});
+			uploadingButton.addEventListener("click", cancelUpload);
+			xhr.upload.addEventListener("progress", function(evt) {
+				if (evt.lengthComputable) {
+					var percentComplete = evt.loaded / evt.total;
+					percentComplete = parseInt(percentComplete * 100);
+					document.getElementById("progress-percentage").innerHTML = percentComplete + "%";
+					document.getElementById("filled-bar").style.width = percentComplete + "%";
+					
+					if (percentComplete === 100) {
+						document.getElementById("progress-percentage").innerHTML = "Finishing up..";
+					}
+					
+				}
+			}, false);
+			
+			return xhr;
+		},
         type: "POST",
         data: formData,
         cache       : false,
@@ -10,12 +40,14 @@ function submitForm(e){
         processData : false,
         url: 'jx.php',
         success: function(data, textStatus, jqXHR) {
-            var jsData = JSON.parse(data)
+            var jsData = JSON.parse(data);
             if(jsData.bOk){
 				document.getElementById('uploadForm').innerHTML = jsData.sOut;
+				//document.getElementById("progress-percentage").innerHTML = "Complete!";
             }
             else{
             	document.getElementById('uploadForm').innerHTML = "<div class='alert alert-danger'>"+jsData.sErr+"</div>"
+            	document.getElementById("progress-percentage").innerHTML = "Error";
             }
         },
         error: function(jqXHR, status, error) {
@@ -27,4 +59,10 @@ function submitForm(e){
 
 function resetForm(){
 	document.getElementById('uploadForm').innerHTML = upload;
+}
+
+function cancelUpload(e) {
+	e.preventDefault();
+	currentUpload.abort();
+	resetForm();
 }
