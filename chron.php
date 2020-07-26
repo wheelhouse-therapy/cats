00899026875ad702f6d88a8ef697edf40a5b530b
bobwildfong <bob@seeds.ca>
2020-07-26 15:56:21 -0400

    changes made on live installation, committed to repo

--- chron.php
+++ chron.php
@@ -2,7 +2,6 @@
 require_once '_start.php';
 require_once CATSLIB.'email_processor.php';
 require_once CATSLIB.'AkauntingHook.php';
-
 $receiptsProcessor = new ReceiptsProcessor($email_processor['emailServer'],$email_processor['receiptsEmail'], $email_processor["receiptsPSW"]);
 AkauntingHook::login($email_processor['akauntingUSR'],$email_processor['akauntingPSW'],$email_processor['akauntingServer']);
 $receiptsProcessor->processEmails();
