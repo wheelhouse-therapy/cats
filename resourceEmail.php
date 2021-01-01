264ccffc1a0fc2de8abb49183e9e6587cb5b0dde
elw-arduino <minecraftisawsome007+github@gmail.com>
2021-01-01 15:37:00 -0500

    Added Prints

--- resourceEmail.php
+++ resourceEmail.php
@@ -5,10 +5,27 @@ const template = <<<ResourceTemplate
 [[TITLE]] --- Uploaded By: [[UPLOADER]]\n
 ResourceTemplate;
 
+function output(String $msg){
+    echo "{$msg}<br />";
+}
+
 $recordBody = "";
+
+$counter = 0;
 
 foreach (ResourceRecord::GetResourcesByNewness($oApp) as $oRR){
     $recordBody .= str_replace(["[[TITLE]]","[[UPLOADER]]"], [$oRR->getFile(),$oRR->getUploader(true)['realname']], template);
+    $counter++;
+}
+
+if($counter == 0){
+    output("No New Resources Found");
+}
+else if($counter == 1){
+    output("{$counter} New Resource Found");
+}
+else{
+    output("{$counter} New Resources Found");
 }
 
 $bodyText = <<<BodyText
@@ -23,5 +40,10 @@ BodyText;
 $bodyText = str_replace("[[RecordBody]]",$recordBody, $bodyText);
 
 $to = "cheshire@catherapyservices.ca";
-
-SEEDEmailSend(["resources@catherapyservices.ca","CATS Resource System"], "", "New resources for you!", $bodyText,"",['reply-to'=>"developer@catherapyservices.ca","bcc"=>[$to]]);
+if($counter > 0){
+    SEEDEmailSend(["resources@catherapyservices.ca","CATS Resource System"], "", "New resources for you!", $bodyText,"",['reply-to'=>"developer@catherapyservices.ca","bcc"=>[$to]]);
+    output("Email Sent");
+}
+else{
+    output("Email Not Sent");
+}
