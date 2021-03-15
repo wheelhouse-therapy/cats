dcc97911e5412b8ce5a3c9f370e280024eada0f6
JoeWildfong <wildfong.joe@gmail.com>
2021-03-14 21:14:05 -0400

    Sidebar improvements for performance - if something breaks, this is probably the problem

--- w/js/sidebar.js
+++ w/js/sidebar.js
@@ -1,27 +1,34 @@
 var tabData;
 var activeTab, tabContent;
+var sidebarDiv;
+var sidebarHeader;
+var selected;
+var tabs;
 function getForm(id) {
-	var selected = document.querySelector(".selected");
+	var clicked = document.querySelector("[data-id=" + id + "]");
+	if (clicked === selected) {
+		return;
+	}
 	if (selected) {
 		selected.classList.remove("selected");
+		selected = null;
 	}
-	var clicked = document.querySelector("[data-id=" + id + "]");
 	if (clicked) {
 		clicked.classList.add("selected");
+		selected = clicked;
 	}
-	var sidebar = document.getElementById("sidebar");
-	sidebar.classList.remove("open");
-	sidebar.setAttribute("data-open-id",id);
+	sidebarDiv.classList.remove("open");
+	sidebarDiv.setAttribute("data-open-id",id);
 	var xhttp = new XMLHttpRequest();
 	xhttp.onreadystatechange = function() {
 		if (this.readyState == 4 && this.status == 200) {
 			var responseText = JSON.parse(this.responseText);
 			if (responseText.bOk == true) {
 				loadSidebar(responseText.raOut);
-				sidebar.classList.add("open");
+				sidebarDiv.classList.add("open");
 			}
 			else{
-				sidebar.removeAttribute("data-open-id");
+				sidebarDiv.removeAttribute("data-open-id");
 			}
 		}
 	};
@@ -30,35 +37,34 @@ function getForm(id) {
 }
 
 function reloadForm(){
-	var sidebar = document.getElementById("sidebar");
-	if(sidebar.classList.contains("open")){
-		getForm(sidebar.getAttribute("data-open-id"));
+	if(sidebarDiv.classList.contains("open")){
+		getForm(sidebarDiv.getAttribute("data-open-id"));
 	}
 }
 function closeSidebar() {
-	document.getElementById("sidebar").classList.remove("open");
-	if(document.querySelector(".selected")){
-		document.querySelector(".selected").classList.remove("selected");
+	sidebarDiv.classList.remove("open");
+	if (selected){
+		selected.classList.remove("selected");
+		selected = null;
 	}
 }
 
 function loadSidebar(data) {
-	var header = document.getElementById("sidebar-header");
-	header.innerHTML = data.header;
+	sidebarHeader.innerHTML = data.header;
 	tabData = data.tabs;
-	var tabs = document.querySelectorAll(".tab");
 	// loop through all the tabs, giving the onclicks if we have HTML for them
 	// and hiding them otherwise
 	for (var i = 0; i < tabs.length; i++) {
 		if (tabData[tabs[i].id]) {
 			tabs[i].addEventListener("click", changeTab);
+			tabs[i].style.display = "";
 		} else {
+			tabs[i].removeEventListener("click", changeTab);
 			tabs[i].style.display = "none";
 		}
 		// set the tab name based on the AJAX return also
 		tabs[i].innerHTML = data.tabNames[i];
 	}
-	activeTab = document.querySelector(".active-tab");
 	if (!activeTab) {
 		// if for some reason there's no active tab
 		// make the first tab active
@@ -72,7 +78,6 @@ function loadSidebar(data) {
 		tabs[0].classList.add("active-tab");
 		activeTab = tabs[0];
 	}
-	tabContent = document.getElementById("tab-content");
 	tabContent.innerHTML = tabData[activeTab.id];
 }
 function changeTab(e) {
@@ -82,3 +87,10 @@ function changeTab(e) {
 	activeTab = e.target;
 	tabContent.innerHTML = tabData[name];
 }
+
+window.addEventListener("DOMContentLoaded", function() {
+	sidebarDiv = document.getElementById("sidebar");
+	sidebarHeader = document.getElementById("sidebar-header");
+	tabs = document.querySelectorAll(".tab");
+	tabContent = document.getElementById("tab-content");
+});
