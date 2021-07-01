2e50392a198f74211550be6efa96d1ddd59223a3
elw-arduino <minecraftisawsome007+github@gmail.com>
2021-07-01 14:42:41 -0400

    Added check for Imagick extension

--- p.php
+++ p.php
@@ -1,2 +1,9 @@
 <?php
+if( class_exists( 'Imagick' ) ) {
+    // Imagick class is installed
+    echo "Imagick Extension installed<br />";
+}
+else{
+    echo "Imagick Extension not installed<br />";
+}
 phpinfo();
