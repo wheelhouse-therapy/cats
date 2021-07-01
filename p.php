8d2ebc2721b7bd25e73fca85bec0aa58b3168e69
elw-arduino <minecraftisawsome007+github@gmail.com>
2021-07-01 15:12:37 -0400

    Removed check, its not needed

--- p.php
+++ p.php
@@ -1,9 +1,2 @@
 <?php
-if( class_exists( 'Imagick' ) ) {
-    // Imagick class is installed
-    echo "Imagick Extension installed<br />";
-}
-else{
-    echo "Imagick Extension not installed<br />";
-}
 phpinfo();
